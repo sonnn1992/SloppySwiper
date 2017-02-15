b3637a4906be8bd1282e37e86370278ff446558d
Son Nguyen <sonnn1992@gmail.com>
2017-02-15 17:58:32 +0700

    no message

---
+++ Example/SSWAnimator.h
@@ -0,0 +1,16 @@
+//
+//  SSWAnimator.h
+//
+//  Created by Arkadiusz Holko http://holko.pl on 29-05-14.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+// Undocumented animation curve used for the navigation controller's transition.
+FOUNDATION_EXPORT UIViewAnimationOptions const SSWNavigationTransitionCurve;
+
+
+@interface SSWAnimator : NSObject <UIViewControllerAnimatedTransitioning>
+
+@end
---
+++ Example/SloppySwiper/SloppySwiper.h
@@ -0,0 +1,19 @@
+//
+//  SloppySwiper.h
+//  SloppySwiper
+//
+//  Created by Son Nguyen on 2/15/17.
+//  Copyright © 2017 Arkadiusz Holko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SloppySwiper.
+FOUNDATION_EXPORT double SloppySwiperVersionNumber;
+
+//! Project version string for SloppySwiper.
+FOUNDATION_EXPORT const unsigned char SloppySwiperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SloppySwiper/PublicHeader.h>
+
+
